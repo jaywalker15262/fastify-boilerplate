a52cfa2ce2774c998dace111d6419169cddedbf8
Jay <jaywalkercooks@gmail.com>
2025-04-23 15:27:58 +0200

    feat: oops

--- src/modules/auth/login/login.route.ts
+++ src/modules/auth/login/login.route.ts
@@ -1,13 +1,19 @@
-import { FastifyInstance, FastifyRequest } from 'fastify';
-import { loginSchema, loginResponseSchema, LoginSchema } from './login.schema';
-import { loginCommand, LoginCommandResult } from './login.handler';
 import { parseJwtExpires } from '../token';
+import { loginCommand, LoginCommandResult } from './login.handler';
+import { loginSchema, loginResponseSchema, LoginSchema } from './login.schema';
 import env from '@/config/env';
+import { FastifyInstance, FastifyRequest } from 'fastify';
 
 export default async function loginRoute(server: FastifyInstance) {
   server.post(
     '/',
     {
+      config: {
+        rateLimit: {
+          max: 5,
+          timeWindow: '10m',
+        },
+      },
       schema: {
         body: loginSchema,
         response: {
@@ -15,16 +21,13 @@ export default async function loginRoute(server: FastifyInstance) {
         },
       },
     },
-    async (
-      request: FastifyRequest<{ Body: LoginSchema }>,
-      reply
-    ) => {
+    async (request: FastifyRequest<{ Body: LoginSchema }>, reply) => {
       const { email, password } = request.body;
 
       const { accessToken, refreshToken }: LoginCommandResult =
-        await request.diScope.resolve('commandBus').execute(
-          loginCommand({ email, password })
-        );
+        await request.diScope
+          .resolve('commandBus')
+          .execute(loginCommand({ email, password }));
 
       const expiresIn = parseJwtExpires(env.jwt.expiresIn);
 
@@ -33,6 +36,6 @@ export default async function loginRoute(server: FastifyInstance) {
         refreshToken,
         expiresIn,
       });
-    }
+    },
   );
 }
